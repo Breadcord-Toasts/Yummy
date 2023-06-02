470205744bcfd247dbb81dfa33c6d75d110adfc4
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-06-02 14:26:01 +0200

    Hopefully make the dnd_dice command more safe

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 import random
 import re
+import ast
+import operator
+from typing import Any
 
+import discord
 from discord.ext import commands
+from discord import app_commands
 
 import breadcord
 
@@ -12,9 +17,41 @@ def dice_to_num(dice_roll: str) -> int | str:
 
     roll_max = int(match["roll_max"])
     multiplier = int(match["multiplier"] or 1)
-    roll_sum = sum(random.randint(1, roll_max) for _ in range(multiplier))
+    return sum(random.randint(1, roll_max) for _ in range(multiplier))
 
-    return roll_sum
+
+def eval_math(expression: str) -> Any:
+    # Based on https://stackoverflow.com/a/9558001
+
+    def limited_pow(a: float, b: float) -> float:
+        if any(abs(n) > 10**4 for n in (a, b)):
+            raise ValueError("Too large for exponentiation.")
+        return operator.pow(a, b)
+
+    operators = {
+        ast.Add: operator.add,
+        ast.Sub: operator.sub,
+        ast.Mult: operator.mul,
+        ast.Div: operator.truediv,
+        ast.FloorDiv: operator.floordiv,
+        ast.Pow: limited_pow,
+        ast.USub: operator.neg,
+        ast.Mod: operator.mod,
+    }
+
+    # noinspection PyTypeChecker
+    # I'm sure it's finneee
+    def eval_(node):
+        if isinstance(node, ast.Num):  # <number>
+            return node.n
+        elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+            return operators[type(node.op)](eval_(node.left), eval_(node.right))
+        elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+            return operators[type(node.op)](eval_(node.operand))
+        else:
+            raise TypeError(node)
+
+    return eval_(ast.parse(expression, mode='eval').body)
 
 
 class Yummy(breadcord.module.ModuleCog):
@@ -25,7 +62,7 @@ class Yummy(breadcord.module.ModuleCog):
         await ctx.reply(f"Rolled a {random.randint(value, max_value)}! 🎲")
 
     @commands.hybrid_command(description='Roll a die using dice notation')
-    async def dnd_dice(self, ctx: commands.Context, *, dice_type: str = "1d6"):
+    async def dnd_dice(self, ctx: commands.Context, *, dice_type: str):
         """
         Example of dice notation: "1d2", "1d20", "5d10", "1d20 - 2d6", "5d8 * 1d5".
         See https://en.wikipedia.org/wiki/Dice_notation for more information
@@ -39,8 +76,24 @@ class Yummy(breadcord.module.ModuleCog):
         )
         math_expr = re.sub(f"[^0-9{accepted_math_operations}]", "", "".join(map(str, dice)))
 
-        #TODO: stop being stupid lmao
-        await ctx.reply(eval(math_expr))
+        try:
+            print(math_expr)
+            out = eval_math(math_expr)
+        except Exception as error:
+            await ctx.reply(f"Could not evaluate.\n{discord.utils.escape_markdown(str(error))}")
+            return
+        else:
+            discord_message_limit = 2000
+            try:
+                if len(out := str(out)) > discord_message_limit:
+                    out = f"{out[:discord_message_limit - 3]}..."
+                await ctx.reply(out)
+            except ValueError as error:
+                # In vase converting the number to a string fails due to its size
+                if re.match(r"^Exceeds the limit \(\d+\) for integer string conversion", error.args[0]):
+                    await ctx.reply("Output too large, could not send.")
+                    return
+                raise error
 
 
 async def setup(bot: breadcord.Bot):
